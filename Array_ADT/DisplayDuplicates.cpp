791b3e3f84e08412556d034f58bcfac58d967c6c
kishankant <chintz.786agchez@gmail.com>
2022-08-13 23:29:58 +0530

    changed length

--- Array_ADT/DisplayDuplicates.cpp
+++ Array_ADT/DisplayDuplicates.cpp
@@ -52,7 +52,7 @@ int main()
     unsigned int size;
     while(1)
     {
-        cout<<"Enter the array size: ";
+        cout<<"Enter the array length: ";
         cin>>size;
         if(size > 0)
             break;
